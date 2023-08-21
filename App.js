5f7a80f0d6f502f092d8e3aaead6e4cec683e3a3
Vlad Surzhko <117448305+VladSurzhko@users.noreply.github.com>
2023-08-21 19:19:37 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, ImageBackground, Keyboard } from "react-native";
+import { Registration } from "./screens/RegistrationScreen";
+import { Login } from "./screens/LoginScreen";
+import { useFonts } from "expo-font";
+import { TouchableWithoutFeedback } from "react-native";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./image/PhotoBG.jpg")}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <Registration />
+          {/* <Login/> */}
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+});
+
